d73d8d16500a43cf58548fac560416142c015004
Crazydevhil <raunakmsgs@gmail.com>
2026-06-24 12:10:48 +0530

    Fix: Add HTMLSelectElement to TypeScript event handler

--- src/app/submit/page.tsx
+++ src/app/submit/page.tsx
@@ -56,7 +56,7 @@ export default function SubmitPage() {
     }
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
